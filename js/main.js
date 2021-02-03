3fc8aabf16a2b8e305a342aeb2b5511ea980d430
Ivan <sorokkahtml@gmail.com>
2021-02-03 23:57:44 +0300

    Added two functions

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Задание 1
+// Ссылка на источник https://developer.mozilla.org/
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (min < 0) {
+    alert('Только неотрицательные значения');
+    return false;
+  }
+
+  if (max <= min) {
+    alert('Парамерт max не может быть меньше или равен параметру min');
+    return false;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Задание 2
+
+function getRandomFloatingPointNumber(min, max, floatingPoint) {
+
+  if (min < 0) {
+    alert('Только неотрицательные значения');
+    return false;
+  }
+
+  if (max <= min) {
+    alert('Парамерт max не может быть меньше или равен параметру min');
+    return false;
+  }
+
+  return ((Math.random() * (max - min) + min).toFixed(floatingPoint));
+}
