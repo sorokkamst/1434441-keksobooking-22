bad50f8a7ffd0b869fa643021ec1e548ef9ff3d7
Ivan <sorokkahtml@gmail.com>
2021-02-04 21:56:42 +0300

    Add validation check error message as a variable

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 'use strict';
-// Задание 1
-// Ссылка на источник https://developer.mozilla.org/
 
+// Вспомогательные функции и переменные
 const validationCheck = (min, max) => {
   if (min < 0 || max <= min) {
-    return false;
+    return true;
   }
 }
+
+const validationCheckErrMsg = 'Только неотрицательные значения\nА также парамерт max не может быть меньше или равен параметру min';
+
+// Задание 1
+// Ссылка на источник https://developer.mozilla.org/
 
 const getRandomInt = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (validationCheck(min, max) === false) {
-    return ('Только неотрицательные значения\nА также парамерт max не может быть меньше или равен параметру min');
+  if (validationCheck(min, max)) {
+    return validationCheckErrMsg;
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -25,8 +29,8 @@ getRandomInt(1,2);
 
 const getRandomFloatingPointNumber = function (min, max, floatingPoint) {
 
-  if (validationCheck(min, max) === false) {
-    return ('Только неотрицательные значения\nА также парамерт max не может быть меньше или равен параметру min');
+  if (validationCheck(min, max)) {
+    return validationCheckErrMsg;
   }
 
   return ((Math.random() * (max - min) + min).toFixed(floatingPoint)/1);
