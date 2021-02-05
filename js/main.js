4ce9ece813fb130a8ad9ec3627e99b11ecc83352
Ivan <sorokkahtml@gmail.com>
2021-02-05 13:23:40 +0300

    change const name/validation check return

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
 // Вспомогательные функции и переменные
+const VALIDATION_CHECK_ERR_MSG = 'Только неотрицательные значения\nА также парамерт max не может быть меньше или равен параметру min';
+
 const validationCheck = (min, max) => {
   if (min < 0 || max <= min) {
-    return true;
+    return 'isInvalid';
   }
 }
-
-const validationCheckErrMsg = 'Только неотрицательные значения\nА также парамерт max не может быть меньше или равен параметру min';
 
 // Задание 1
 // Ссылка на источник https://developer.mozilla.org/
@@ -16,8 +16,8 @@ const getRandomInt = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (validationCheck(min, max)) {
-    return validationCheckErrMsg;
+  if (validationCheck(min, max) === 'isInvalid') {
+    return VALIDATION_CHECK_ERR_MSG;
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -29,11 +29,11 @@ getRandomInt(1,2);
 
 const getRandomFloatingPointNumber = function (min, max, floatingPoint) {
 
-  if (validationCheck(min, max)) {
-    return validationCheckErrMsg;
+  if (validationCheck(min, max) === 'isInvalid') {
+    return VALIDATION_CHECK_ERR_MSG;
   }
 
-  return ((Math.random() * (max - min) + min).toFixed(floatingPoint)/1);
+  return +(Math.random() * (max - min) + min).toFixed(floatingPoint);
 }
 
 getRandomFloatingPointNumber(1,2,1);
