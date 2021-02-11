69e83e29f7ad2d6374a7b02b34dd15184a3d67e4
Ivan <sorokkahtml@gmail.com>
2021-02-11 17:14:25 +0300

    Module3-task1 update

--- js/main.js
+++ js/main.js
@@ -38,58 +38,61 @@ getRandomFloatingPointNumber(1,2,1);
 // Module3-task1
 
 // Input data
+const TITLE = ['Информация о поездке', 'Описание предложения', 'Информация о месте проживания'];
 const ACCOMODATION_TYPE = ['palace', 'flat', 'house', 'bungalow'];
-const ARRIVAL_TIME = ['12:00', '13:00', '14:00'];
-const DEPARTURE_TIME = ['12:00', '13:00', '14:00'];
+const TIME = ['12:00', '13:00', '14:00'];
 const ACCOMMODATION_FACILITIES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const DESCRIPTION = ['Чисто, убрано, с крутым балконом', 'Большие номера с видом на море', 'Френдли к животным', 'Панорамные окна для фоточек']
 const ACCOMODATION_VARIANTS = 10;
-// Secondary variables
 
-// let result = [];
-//
-// function checkAvailability(arr, val) {
-//   return arr.some(arrVal => val === arrVal);
-// }
-//
-// const RANDOM_MASSIV = (ACCOMMODATION_FACILITIES, ACCOMMODATION_FACILITIES[getRandomInt(0, ACCOMMODATION_FACILITIES.length)]) = > {
-//   for (let i = 0; i < arr.length; i++ ) {
-//     let randomValue = ACCOMMODATION_FACILITIES[getRandomInt(0, ACCOMMODATION_FACILITIES.length - 1)];
-//     if (checkAvailability(arr, value)) ? true : result.push(randomValue);
-//   }
-// }
-//   console.log(result);
+// Secondary functions
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5)
+}
 
-const author = () => {
+const getRandomAvatarNumber = (minNumber,maxNumber) => {
+  return ['0'+ getRandomInt(minNumber, maxNumber)].join();
+};
+
+const getRandomArrayIndex = (arr) => {
+  return arr[getRandomInt(0, arr.length - 1)]
+};
+
+const getRandomArray = (arr) => {
+  shuffle(arr);
+  const slicedArray = arr.slice(0, getRandomInt(1, arr.length - 1));
+  return slicedArray;
+};
+
+// Result function
+const generateOffer = () => {
+
+  const locationX = getRandomFloatingPointNumber(35.65000, 35.70000, 5);
+  const locationY = getRandomFloatingPointNumber(139.70000, 139.80000, 5);
+
   return {
-    avatar: 'img/avatars/user' + [0, getRandomInt(1,8)].join('') + '.png',
+    'author': {
+      avatar: 'img/avatars/user' + getRandomAvatarNumber(1,8) + '.png',
+    },
+    'offer': {
+      title: getRandomArrayIndex(TITLE),
+      address: [locationX, locationY].toString(),
+      price: getRandomInt(0,5000),
+      type: getRandomArrayIndex(ACCOMODATION_TYPE),
+      rooms: getRandomInt(1,100),
+      guests: getRandomInt(1,100),
+      checkin: getRandomArrayIndex(TIME),
+      checkout: getRandomArrayIndex(TIME),
+      features: getRandomArray(ACCOMMODATION_FACILITIES),
+      description: getRandomArrayIndex(DESCRIPTION),
+      photos: getRandomArray(PHOTOS),
+    },
+    'location': {
+      x: locationX,
+      y: locationY,
+    },
   };
 };
 
-const offer = () => {
-  return {
-    title: 'Информация о поездке',
-    address: [getRandomFloatingPointNumber(1,200,1),getRandomFloatingPointNumber(1,200,1)],
-    price: getRandomInt(0,5000),
-    type: ACCOMODATION_TYPE[getRandomInt(0, ACCOMODATION_TYPE.length - 1)],
-    rooms: getRandomInt(1,1000),
-    guests: getRandomInt(1,1000),
-    checkin: ARRIVAL_TIME[getRandomInt(0, ARRIVAL_TIME.length - 1)],
-    checkout: DEPARTURE_TIME[getRandomInt(0, DEPARTURE_TIME.length - 1)],
-    // features:
-    description: 'Чисто, убрано, с крутым балконом',
-    // photos:
-  };
-};
-
-const location = () => {
-  return {
-    x: getRandomFloatingPointNumber(35.65000, 35.70000, 5),
-    y: getRandomFloatingPointNumber(139.70000, 139.80000, 5),
-  };
-};
-
-const randomLocation = () => Object.assign({}, author(), offer(), location());
-
-const tenRandomLocations = new Array(ACCOMODATION_VARIANTS).fill(null).map(() => randomLocation());
-console.log(tenRandomLocations);
+const getRandomLocations = new Array(ACCOMODATION_VARIANTS).fill(null).map(() => generateOffer());
